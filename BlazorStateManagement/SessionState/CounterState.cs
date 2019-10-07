6f58a70ba34cc7acefe2bd46360db453d6ff8a64
emilio barrera <barrera_emilio@hotmail.com>
2019-10-07 14:41:20 +0100

    Add project files.

---
+++ BlazorStateManagement/SessionState/CounterState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorStateManagement.SessionState
+{
+    public class CounterState
+    {
+        private int _currentCount = 0;
+
+        public EventHandler StateChange;
+        
+        public int GetCurretCount()
+        {
+            return _currentCount;
+        }
+
+        public void SetCurrentCount(int paramCount)
+        {
+            _currentCount = paramCount;
+            StateHashanged();
+        }
+
+        public void ResetCurrentCount()
+        {
+            _currentCount = 0;
+            StateHashanged();
+        }
+
+        private void StateHashanged()
+        {
+            StateChange?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
